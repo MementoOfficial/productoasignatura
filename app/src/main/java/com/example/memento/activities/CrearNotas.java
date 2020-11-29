81eb7bc8db51fc849aeea83867a8c2ab3dc943a1
Jeams <70823683+Jeamslab@users.noreply.github.com>
2020-11-28 23:01:05 -0600

    Alarm added to full project

--- app/src/main/java/com/example/memento/activities/CrearNotas.java
+++ app/src/main/java/com/example/memento/activities/CrearNotas.java
@@ -1,35 +1,54 @@
 package com.example.memento.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.os.SystemClock;
+import android.widget.Toast;
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.DateFormat;
+import android.icu.util.Calendar;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
 
 import com.example.memento.R;
+import com.example.memento.alarms.AlertReceiver;
+import com.example.memento.alarms.TimePickerFragment;
 import com.example.memento.database.NotesDatabase;
 import com.example.memento.entidades.Note;
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
-public class CrearNotas extends AppCompatActivity {
-
+public class CrearNotas extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
+    private TextView mTextView;
     private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
     private TextView textDateTime;
+    private int i;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_notas);
 
+
+        mTextView = findViewById(R.id.tv_test);
         ImageView imageBack = findViewById(R.id.imageBack);
 
         imageBack.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +56,8 @@ public class CrearNotas extends AppCompatActivity {
             public void onClick(View v) {
                 onBackPressed();
             }
-        });
+        })
+        ;
 
         inputNoteTitle = findViewById(R.id.inputNoteTitle);
         inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
@@ -55,7 +75,26 @@ public class CrearNotas extends AppCompatActivity {
             public void onClick(View v) {
                 saveNote();
             }
+
+
         });
+
+        Button buttonTimePicker = findViewById(R.id.button_timepicker);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+        Button buttonCancelAlarm = findViewById(R.id.button_cancel);
+        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+
     }
 
     private void saveNote(){
@@ -94,4 +133,57 @@ public class CrearNotas extends AppCompatActivity {
 
         new SaveNoteTask().execute();
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+        updateTimeText(c);
+        startAlarm(c);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateTimeText(Calendar c) {
+        String timeText = "Alarma establecida a: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+
+        mTextView.setText(timeText);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        for (i = 0; i < 10 ; ++i) {
+            Intent intent = new Intent(this, AlertReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, i, intent, 0);
+
+            if (c.before(Calendar.getInstance())) {
+                c.add(Calendar.DATE, i);
+                //alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                //       SystemClock.elapsedRealtime() + 60000 * i,
+                //       pendingIntent);
+
+            }
+
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+
+        }
+    }
+
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+        alarmManager.cancel(pendingIntent);
+
+        mTextView.setText("Alarma cancelada");
+
+    }
+
 }
