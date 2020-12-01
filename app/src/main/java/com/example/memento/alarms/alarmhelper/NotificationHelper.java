ff49a147f27fe77186c39572ed580232643542a3
hasser9518 <52793236+hasser9518@users.noreply.github.com>
2020-12-01 00:35:21 -0600

    Image, View and Update

--- app/src/main/java/com/example/memento/alarms/alarmhelper/NotificationHelper.java
+++ app/src/main/java/com/example/memento/alarms/alarmhelper/NotificationHelper.java
@@ -6,35 +6,32 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Build;
+import com.example.memento.R;
 
 import androidx.core.app.NotificationCompat;
 
-import com.example.memento.R;
 
 public class NotificationHelper extends ContextWrapper {
     public static final String channelID = "channelID";
     public static final String channelName = "Channel Name";
     private NotificationManager mManager;
-
     public NotificationHelper(Context base) {
         super(base);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createChannel();
         }
     }
-    @TargetApi(Build.VERSION_CODES.0)
+    @TargetApi(Build.VERSION_CODES.O)
     private void createChannel() {
         NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
         getManager().createNotificationChannel(channel);
     }
-
     public NotificationManager getManager() {
-        if(mManager == null) {
+        if (mManager == null) {
             mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         }
         return mManager;
     }
-
     public NotificationCompat.Builder getChannelNotification() {
         return new NotificationCompat.Builder(getApplicationContext(), channelID)
                 .setContentTitle("Heyy!")
