6f35f98b235bb4543134316c7887cdf69972f74e
hasser9518 <52793236+hasser9518@users.noreply.github.com>
2020-12-01 00:11:09 -0600

    Image, View and Update

--- app/src/main/java/com/example/memento/alarms/AlertReceiver.java
+++ app/src/main/java/com/example/memento/alarms/AlertReceiver.java
@@ -1,4 +1,20 @@
 package com.example.memento.alarms;
 
-public class AlertReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.memento.alarms.alarmhelper.NotificationHelper;
+
+import java.util.ArrayList;
+
+public class AlertReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
+    }
 }
--- app/src/main/java/com/example/memento/alarms/alarmhelper/NotificationHelper.java
+++ app/src/main/java/com/example/memento/alarms/alarmhelper/NotificationHelper.java
@@ -1,4 +1,42 @@
 package com.example.memento.alarms.alarmhelper;
 
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.memento.R;
+
 public class NotificationHelper {
+    public static final String channelID = "channelID";
+    public static final String channelName = "Channel Name";
+    private NotificationManager mManager;
+    public NotificationHelper(Context base) {
+        super(base);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
+            createChannel();
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.0)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if(mManager == null) {
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification() {
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle("Heyy!")
+                .setContentText("El tiempo que has establecido a tu tarea ha terminado.")
+                .setSmallIcon(R.drawable.ic_notification);
+    }
 }
